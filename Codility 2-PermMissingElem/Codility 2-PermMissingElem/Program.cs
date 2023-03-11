12c7b76f5ac26703dcb488553e11ebc71157a4f3
M-racle <108946729+M-racle@users.noreply.github.com>
2023-03-11 17:39:23 +0300

    Codility 2-PermMissingElem
    Find missing number in array

---
+++ Codility 2-PermMissingElem/Codility 2-PermMissingElem/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+int[] A = new int[] { 2, 1, 5, 3 };
+
+int B = A.Length + 1;
+
+int sum = B * (B + 1) / 2;
+for (int i = 0; i < A.Length; i++)
+{
+	sum -= A[i];
+}
+Console.WriteLine("Missing number= " + sum);
