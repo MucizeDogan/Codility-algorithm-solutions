46b41f4913c967392b20cd20d745c9b27e4202db
M-racle <108946729+M-racle@users.noreply.github.com>
2023-03-11 17:44:11 +0300

    Codility 3-PassingCars
    Passing Cars solution with c#

---
+++ Codility 3-PassingCars/Codility 3-PassingCars/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int[] A = new int[] {0,1,0,1,1};
+
+int eastBound = 0;
+
+int westBound = 0;
+
+for(int i = 0; i < A.Length; i++)
+{
+    if(A[i] == 0)
+        eastBound++;
+
+
+    if(A[i] == 1)
+        westBound+=eastBound;
+
+}
+
+Console.WriteLine("number of passing vehicles: " + westBound);
+
+
+
+
+
+
+
+
+
+
+
