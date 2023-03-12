34a1c785bf0919101392f0204c99c91da2be2ff7
M-racle <108946729+M-racle@users.noreply.github.com>
2023-03-12 13:54:46 +0300

    Codility 5-OddOccurencesInArray

---
+++ Codility 5-OddOccurencesInArray/Codility 5-OddOccurencesInArray/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int[] A = new int[] { 9, 3, 9, 3, 9, 9, 9, 7, 7 };
+
+
+HashSet<int> hashset = new HashSet<int>();
+
+int unique = 0;
+
+
+for (int i = 0; i < A.Length; i++)
+{
+    if (hashset.Equals(A[i]))
+    {
+       
+        hashset.Remove(A[i]);
+
+    }
+    else
+    {
+       
+        hashset.Add(A[i]);
+
+        unique = A[i];
+
+    }
+
+
+
+}
+
+
+Console.WriteLine("Result: " + unique);
+
+
+
+
+
