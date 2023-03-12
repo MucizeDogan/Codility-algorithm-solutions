f64994cfd588b1ff0dbdd20364902cf53ca8e879
M-racle <108946729+M-racle@users.noreply.github.com>
2023-03-12 13:58:31 +0300

    Codility 6-Distinct

---
+++ Codility 6-Distinct/Codility 6-Distinct/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+int[] A = new int[] { 2, 1, 1, 2, 3, 1 };
+
+
+int result = 0;
+
+
+
+HashSet<int> set = new HashSet<int>();
+
+
+for (int i = 0; i < A.Length; i++)
+{
+   
+    set.Add(A[i]);
+
+
+}
+
+result = set.Count();
+
+
+Console.WriteLine("Result= " + result);
