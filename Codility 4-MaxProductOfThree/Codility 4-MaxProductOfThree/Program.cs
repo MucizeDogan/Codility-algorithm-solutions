b06c16b7a36e271734c3036530971ae123b1f93b
M-racle <108946729+M-racle@users.noreply.github.com>
2023-03-12 13:53:19 +0300

    Codility 4-MaxProductOfThree

---
+++ Codility 4-MaxProductOfThree/Codility 4-MaxProductOfThree/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[] A = new int[] { -3, 1, 2, -2, 5, 6 };
+
+Array.Sort(A);
+
+
+for (int i = 0; i < A.Length; i++)  //to show the array in a sorted way
+    {
+
+    Console.WriteLine(A[i]);
+
+    }
+    
+
+int result3 = 0;
+
+int result1 = A[A.Length - 1] * A[A.Length - 2] * A[A.Length - 3];
+
+int result2 = A[0] * A[1] * A[A.Length - 1];
+
+result3 = Math.Max(result1, result2);
+
+
+Console.WriteLine("Result = " + result3);
+
+
+
+
