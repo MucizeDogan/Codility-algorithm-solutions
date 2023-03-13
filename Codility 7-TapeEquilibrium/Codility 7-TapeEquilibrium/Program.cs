df8832296daf8735249896ee7b26b8b9bc07c134
M-racle <108946729+M-racle@users.noreply.github.com>
2023-03-13 15:29:23 +0300

    Codility 7-TapeEquilibrium

---
+++ Codility 7-TapeEquilibrium/Codility 7-TapeEquilibrium/Program.cs
@@ -0,0 +1,45 @@
+﻿//TapeEquilibrium
+
+
+int[] A = new int[] { 3, 1, 2, 4, 3 };
+
+
+int TotalToplam = 0;
+
+int left = 0;
+int right = 0;
+int minDifference = Int32.MaxValue;
+
+for (int i = 0; i < A.Length; i++)
+{
+  
+    TotalToplam += A[i];
+
+}
+
+
+int result = 0;
+
+
+for (int i = 1; i < A.Length; i++)
+{
+
+    left += A[i];
+    right = TotalToplam - left;
+    
+    result = Math.Abs(left - right);
+    
+    if (result < minDifference)
+        
+        minDifference = result;
+
+}
+Console.WriteLine("Minimum difference: " + minDifference);
+
+
+
+
+
+
+
+
