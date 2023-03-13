87e6f6a780570f427f5a05a42ec52522839269dd
M-racle <108946729+M-racle@users.noreply.github.com>
2023-03-13 15:30:31 +0300

    Codility 8-FrogRiverOne

---
+++ Codility 8-FrogRiverOne/Codility 8-FrogRiverOne/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+int[] A = new int[] { 1, 3, 1, 4, 2, 3, 5, 4 };
+
+int MaxStep = 5;
+
+int result = 0;
+
+HashSet<int> set = new HashSet<int>();
+
+for (int i = 0; i < A.Length; i++)
+{
+
+    // set.Add(A[i]);
+    if (A[i] == MaxStep)
+
+        result = i;
+
+}
+
+Console.WriteLine("earliest time= " + result);
+
+
+
